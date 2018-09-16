1537c992acbba6921bd217b2eaecf7c18bbb9735
Vinhlee95 <lethanhvinh95@gmail.com>
2018-09-16 12:54:19 +0300

    create bar chart

--- index.js
+++ index.js
@@ -14,3 +14,37 @@ d3.select('body')
   .append('p')
   // method
   .text(function(item) { return item; });
+
+// add a simple bar chat
+// add a dummy dataset
+const charDataset = ['10', '20', '30'];
+
+const svgWidth = 500, svgHeight = 100, barPadding = 5;
+const barWidth = (svgWidth / 7);
+
+// select svg container and define width & height
+const svg = d3.select('svg')
+              .attr('height', svgHeight)
+              .attr('width', svgWidth);
+
+// create bar chart
+const barChart = svg.selectAll('rect')
+    .data(charDataset)
+    .enter()
+    .append('rect')
+    // apply methods to each of the rect
+    .attr('y', function(d) {
+      return svgHeight - d;
+    })
+    .attr('height', function(d) {
+      return d;
+    })
+    .attr('width', barWidth - barPadding)
+    // 
+    .attr('transform', function(d, i) {
+      const translate = [barWidth * i , 0];
+      return "translate(" + translate + ")"
+    })
+
+// apply background color for chart
+d3.selectAll('rect').style('background', 'green');
